2d19a2731d992fbbf772aa45d1d7d4f153482255
João Victor de Andrade <joao.victor@primesquad.com.br>
2022-11-06 20:31:42 -0300

    feat: add get all categories

---
+++ GroceryStoreApi.Dto/CategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GroceryStoreApi.Dto
+{
+    public class CategoryDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ GroceryStoreApi/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using GroceryStoreApi.Dto;
+using GroceryStoreApi.Infra.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroceryStoreApi.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+        public CategoryController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<CategoryDto>> Index()
+        {
+            var categories = _context.Categories.Select(c => new CategoryDto { Id = c.Id, Description = c.Description, Name = c.Name });
+
+            return await categories.ToListAsync();
+        }
+    }
+}
