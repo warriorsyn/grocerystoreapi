dfb4f60d3a5f480fb11cbc4fd65908ba66c8fc98
João Victor de Andrade <joao.victor@primesquad.com.br>
2022-11-06 16:40:07 -0300

    feat: add dto

---
+++ GroceryStoreApi.Dto/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStoreApi.Dto
+{
+    public class Test
+    {
+        public int Id { get; set; }
+    }
+}
